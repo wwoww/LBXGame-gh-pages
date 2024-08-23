89508082802b46e892a96891498cdf66349dfd46
wwoww <woshifish1@163.com>
2024-08-23 15:15:00 +0800

    upload LBXGame-gh-pages

---
+++ src/settings.a1e1d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/loadResScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"cf204",main:"d0184"}};
